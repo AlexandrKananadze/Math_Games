c280cb8d44a830424b520e5dfe9dcc44ab39ca4a
AlexandrKananadze <9223773@mail.ru>
2021-07-18 15:12:09 +0300

    Final optimization4

--- src/main/java/hexlet/code/Engine2.java
+++ src/main/java/hexlet/code/Engine2.java
@@ -43,10 +43,10 @@ public class Engine2 {
         System.out.println("Let's try again, " + userName + "!");
     }
 
-    public static void execute(String task, Map<String, String> question) {
+    public static void execute(String task, Map<String, String> questions) {
         greeting();
         System.out.println(task);
-        for (Map.Entry<String, String> questionToAnswer : question.entrySet()) {
+        for (Map.Entry<String, String> questionToAnswer : questions.entrySet()) {
             outPutTaskToUser(questionToAnswer.getKey());
             String userAnswer = inputAnswerFromUserInt();
             if (isCorrect(userAnswer, questionToAnswer.getValue())) {
