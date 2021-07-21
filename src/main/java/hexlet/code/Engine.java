0fd8cd4f3b7a9e992b2aede893f6e17f2ccd1029
AlexandrKananadze <9223773@mail.ru>
2021-07-21 13:56:10 +0300

    Final optimization5

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Engine {
     public static final int RANGE_RANDOM = 100;
     public static final int NUMBER_OF_ATTEMPTS = 3;
-    private static String userName = null;
 
     public static int genRandom() {
         return (int) (Math.random() * RANGE_RANDOM + 1);
     }
 
-    public static void greeting() {
+    public static String greeting() {
         System.out.println("May I have your name?");
         Scanner console = new Scanner(System.in);
-        userName = console.nextLine();
+        String userName = console.nextLine();
         System.out.println("Hello, " + userName + "!");
+        return userName;
     }
 
     public static void outPutTaskToUser(String taskFromGame) {
@@ -33,13 +33,13 @@ public class Engine {
         return rightAnswer.equals(rightUserAnswer);
     }
 
-    public static void wrongUserAnswerOutput(String rightAnswer, String userAnswer) {
+    public static void wrongUserAnswerOutput(String rightAnswer, String userAnswer, String userName) {
         System.out.println(userAnswer + " is wrong  answer ;(. Correct answer was " + rightAnswer + "!");
         System.out.println("Let's try again, " + userName + "!");
     }
 
     public static void execute(String task, Map<String, String> questions) {
-        greeting();
+        String userName = greeting();
         System.out.println(task);
         for (Map.Entry<String, String> questionToAnswer : questions.entrySet()) {
             outPutTaskToUser(questionToAnswer.getKey());
@@ -47,8 +47,8 @@ public class Engine {
             if (isCorrect(userAnswer, questionToAnswer.getValue())) {
                 System.out.println("Correct!");
             } else {
-                wrongUserAnswerOutput(questionToAnswer.getValue(), userAnswer);
-               return;
+                wrongUserAnswerOutput(questionToAnswer.getValue(), userAnswer, userName);
+                return;
             }
         }
         System.out.println("Congratulations, " + userName + "!");
